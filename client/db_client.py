99d1f17672f7f7051742677b794bddd3f104f052
komikodok <komikodok666@gmail.com>
2025-03-25 22:40:35 +0700

    minor update

--- client/db_client.py
+++ client/db_client.py
@@ -14,28 +14,3 @@ db = firestore.client()
 
 def get_db():
     return db
-
-
-
-# from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-# import os
-# from dotenv import find_dotenv, load_dotenv
-
-# load_dotenv(find_dotenv())
-
-# DATABASE_URL = os.getenv("DATABASE_URL")
-
-# engine = create_engine(DATABASE_URL)
-
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base = declarative_base()
-
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
--- llm/node.py
+++ llm/node.py
@@ -1,17 +1,16 @@
 from langchain_core.pydantic_v1 import Field, BaseModel
 from langchain_groq.chat_models import ChatGroq
 from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
-from langchain.vectorstores.pgvector import PGVector
-from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from langchain_google_firestore.vectorstores import FirestoreVectorStore
+from langchain_core.output_parsers import StrOutputParser
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.documents import Document
 
 from dotenv import load_dotenv, find_dotenv
 import os
 
 from .state import State
-from client.db_client import DATABASE_URL
+from client.db_client import db
 
 load_dotenv(find_dotenv())
 
@@ -20,13 +19,13 @@ model_name = os.getenv("MODEL_NAME")
 llm = ChatGroq(model=model_name, api_key=groq_api_key)
 embeddings = FastEmbedEmbeddings()
 
-vectorstore = PGVector(
-    connection_string=DATABASE_URL,
-    embedding_function=embeddings,
-    collection_name="document_embeddings"
+vectorstore = FirestoreVectorStore(
+    client=db,
+    collection="document_embeddings",
+    embedding_service=embeddings
 )
 retriever = vectorstore.as_retriever()
-    
+
 def retrieve_node(state: State):
     question = state.get("question")
 
